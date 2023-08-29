f48bc0f7f993e6dc02545690b0ef34dfca957c88
TanathonBas <142291067+TanathonBas@users.noreply.github.com>
2023-08-29 12:04:43 +0700

    Add files via upload

---
+++ saupythonproject/saupythonproject/lectrue04/dtitest02.py
@@ -0,0 +1,9 @@
+#function แบบที่ 1 - No parameter/No return
+def showanme():
+    print('Tanathon')
+    print('ธนาธร')
+
+showanme()
+print('Hey')
+showanme()
+#function แบบที่ 2 - Have parameters/No return
