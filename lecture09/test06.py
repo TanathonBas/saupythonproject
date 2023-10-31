d29b9abf6949ba3f7a2371c6fc7cec34353e8937
TanathonBas <142291067+TanathonBas@users.noreply.github.com>
2023-10-31 17:32:42 +0700

    Delete lecture09 directory

--- lecture09/test06.py
+++
@@ -1,7 +0,0 @@
-def sumNumber(n1,n2) :
-    return n1 + n2
-
-def showHi():
-    print('Hi..')
-
-VAT = 0.7
--- lecture09/test08.py
+++
@@ -1,8 +0,0 @@
-import math
-def calSquareArea(wi,io):
-    return wi * io 
-def calTriangleArea(base,high):
-    return base* high / 2 
-
-def calCircleArea(radius):
-    return math.pi * math.pow(radius,2)
